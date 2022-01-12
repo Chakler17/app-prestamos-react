7a4e9bab099c8c1f9a0a26ce45836645f2906ce0
Hugo Arias <hugoarias@wposs.com>
2022-01-12 14:43:37 -0500

    Se cambia el spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,13 +1,11 @@
 import React from "react";
-import './Spinner.css'
+import "./Spinner.css";
 
 const Spinner = () => {
   return (
-    <div className="sk-folding-cube">
-      <div className="sk-cube1 sk-cube"></div>
-      <div className="sk-cube2 sk-cube"></div>
-      <div className="sk-cube4 sk-cube"></div>
-      <div className="sk-cube3 sk-cube"></div>
+    <div class="spinner">
+      <div class="dot1"></div>
+      <div class="dot2"></div>
     </div>
   );
 };
